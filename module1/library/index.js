cd76c69cc666a594a5672948e45db274dd965178
VorgSpring <drmrSP@yandex.ru>
2022-07-06 22:40:46 +0300

    created templates for books

--- module1/library/index.js
+++ module1/library/index.js
@@ -6,24 +6,30 @@ const loggerMiddleware = require('./middleware/logger');
 const errorMiddleware = require('./middleware/error');
 
 const indexRouter = require('./routes/index');
+const booksApiRouter = require('./routes/api/books');
+const userApiRouter = require('./routes/api/user');
+
 const booksRouter = require('./routes/books');
-const userRouter = require('./routes/user');
 
 const app = express();
 
-app.use(bodyParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(loggerMiddleware);
 
-app.use('/public', express.static(__dirname+"/public"));
+app.use('/public', express.static(__dirname + "/public"));
 
 app.use('/', indexRouter);
-app.use('/api/books', booksRouter);
-app.use('/api/user', userRouter);
+app.use('/books', booksRouter);
+app.use('/api/books', booksApiRouter);
+app.use('/api/user', userApiRouter);
 
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
